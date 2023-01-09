bc405ddff745490ceb4df99af189af4b80fc94f1
manana <manana@A13E31>
2023-01-09 13:58:15 +0100

    editado

--- _02_cuentas_bancarias/src/service/CuentaMovimientos.java
+++ _02_cuentas_bancarias/src/service/CuentaMovimientos.java
@@ -1,12 +1,17 @@
 package service;
 
+import java.util.ArrayList;
+
 public class CuentaMovimientos {
 	/*tercera parte:
 	Se creará una nueva clase que registre los movimientos realizados en la cuenta (ingreso y extracción).
 	Cada movimiento se caracteriza por una cantidad y un tipo (ingreso o extracción).
 	Incorporará un nuevo método que devuelva los movimientos registrados*/
 	
+	private ArrayList<movimiento> movimientos;
 	
+	//constructor
+	//y lo que falte...
 	
 	
 
