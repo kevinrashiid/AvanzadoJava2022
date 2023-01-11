79d934e77ae90f54f41689f62051a4d25d25ce64
manana <manana@A13E31>
2023-01-11 09:20:29 +0100

    act desde clase

--- _04_FigurasClasesAbstractas/src/geometria/Triangulo.java
+++ _04_FigurasClasesAbstractas/src/geometria/Triangulo.java
@@ -1,6 +1,16 @@
 package geometria;
 
 public class Triangulo extends Figura{
-	
+	public int base,altura;
 
+	public Triangulo(String color, int base, int altura) {
+		super(color);//llamada a constructor de figura
+		this.base = base;
+		this.altura = altura;
+	}
+
+	@Override
+	public double superficie() {
+		return base*altura/2;
+	}
 }
