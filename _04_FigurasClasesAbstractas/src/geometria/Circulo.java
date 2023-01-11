2c1483ecb6fa3547727e7d90357e6ac528e378d0
kevinrashid <kevinrashid@localhost>
2023-01-11 09:08:35 +0100

    Desde clase

--- _04_FigurasClasesAbstractas/src/geometria/Circulo.java
+++ _04_FigurasClasesAbstractas/src/geometria/Circulo.java
@@ -1,5 +1,14 @@
 package geometria;
 
 public class Circulo extends Figura{
-	
+	private int radio;
+	public Circulo(String color, int radio) {
+		super(color);//llamada a constructor de figura
+		this.radio=radio;
+	}
+	public double superficie() {
+		return Math.PI*radio*radio;
+	}
 }
+
+
