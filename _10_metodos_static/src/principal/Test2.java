d27e8770e993ab7909ae64103c150f808921d7c7
manana <manana@A13E31>
2023-01-12 12:45:08 +0100

    act desde clase

--- _10_metodos_static/src/principal/Test2.java
+++ _10_metodos_static/src/principal/Test2.java
@@ -2,10 +2,10 @@ package principal;
 
 interface Printer{
 	void print(String cad);
-	//incluiremos un metodos static que proporcione
-	//una determinada implementacion de la interfaz
+	//incluiremos un metodos static que proporcione un obejeto
+	//de una clase que implementa esa la interfaz
 	static Printer of() {
-		return new Printer() {//objeto de clase anonima
+		return new Printer() {// creamos objeto de clase anonima
 			@Override
 			public void print(String cad) {
 			System.out.println(cad);	
@@ -17,8 +17,17 @@ interface Printer{
 public class Test2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+			
+		//podemos llamar al metodo estarico de la interfaz
+		//para que nos de un objeto ya hecho de una clase que implementa la interfaz
+		//y asi no tener que crear una clase que la implemente
+		
+		//no creamos un objeto de interface
+		//lo que hacemos es crear un objeto de una subclase de interface
+		
+		Printer pr=Printer.of();
+		pr.print("hello");
+		
 	}
 
 }
---
+++ _10_metodos_static/src/principal/Test3Ejercicio.java
@@ -0,0 +1,15 @@
+package principal;
+
+import java.util.ArrayList;
+
+/*Crear una lista de numeros con los valores 7,15y 9*/
+
+public class Test3Ejercicio {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> num=new ArrayList<>();
+		num.add(7);
+		num.add(15);
+		num.add(9);
+	}
+}
