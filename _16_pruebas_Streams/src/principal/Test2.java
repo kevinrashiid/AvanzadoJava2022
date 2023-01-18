212b92ecdd1347185be6c4cbd173d2ccb99c0749
kevinrashid <kevinrashid@localhost>
2023-01-18 15:15:13 +0100

    desde el mac

--- _16_pruebas_Streams/src/principal/Test2.java
+++ _16_pruebas_Streams/src/principal/Test2.java
@@ -8,13 +8,20 @@ public class Test2 {
 		List<Integer> nums=List.of(6,11,-4,6,-7,327,6,-9,11,41,19);
 		//muestra los primeros 7 numeros de la lista, 
 		//sin contar los duplicados
-		nums.stream()
+		/*nums.stream()
 		.distinct()
 		.limit(7)
-		.forEach(n->System.out.println(n));
+		.forEach(n->System.out.println(n));*/
 		
-		//hay algun negativo??
-		//son todos pares?
+		
+	//hay algun negativo??
+	System.out.println("hay algun numero negativo?? \n"
+	+nums.stream()
+	.anyMatch(n->n<0));
+	
+	//son todos pares?
+	System.out.println("Son todos pares?? \n" 
+	+nums.stream()
+	.allMatch(n->n%2==0));	
 	}
-
 }
