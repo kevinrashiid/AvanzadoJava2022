dc025148b1445185a020a7b737bfda43c81a31cb
kevinrashid <kevinrashid@localhost>
2023-01-28 19:00:46 +0100

    subdio desde el mac

--- _17_logiaDeNegocio/src/model/CityModel.java
+++ _17_logiaDeNegocio/src/model/CityModel.java
@@ -1,12 +1,10 @@
 package model;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Setter
-@Getter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CityModel {
